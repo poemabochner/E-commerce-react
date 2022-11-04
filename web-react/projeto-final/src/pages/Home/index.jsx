916890ce3cfdd8e9e305fa8cef150de728a03c31
BilPinheiro <prod.gabriel.pinheiro@gmail.com>
2022-11-04 19:21:50 -0300

    Modelo Cards de Produto

--- web-react/projeto-final/src/pages/Home/index.jsx
+++ web-react/projeto-final/src/pages/Home/index.jsx
@@ -1,3 +1,47 @@
-export function Home(){
-    return <h1>Home</h1>
+import { CardProduto } from "../../components/CardProduto";
+import "./styles.css";
+export function Home() {
+  return (
+    <div className='fundo'>
+      <header>
+        <h1>header</h1>
+      </header>
+      <div>
+        <h1>Carrossel</h1>
+      </div>
+      <div className="boxDescricao">
+        <h4>Produtos em Destaque</h4>
+      </div>
+      <div className="CardList">
+        <CardProduto />
+        <CardProduto />
+        <CardProduto />
+        <CardProduto />
+        <CardProduto />
+      </div>
+      <div className="boxDescricao">
+        <h4>Ofertas</h4>
+      </div>
+      <div className="CardList">
+        <CardProduto />
+        <CardProduto />
+        <CardProduto />
+        <CardProduto />
+        <CardProduto />
+      </div>
+      <div className="boxDescricao">
+        <h4>Mais pesquisados</h4>
+      </div>
+      <div className="CardList">
+        <CardProduto />
+        <CardProduto />
+        <CardProduto />
+        <CardProduto />
+        <CardProduto />
+      </div>
+      <footer>
+        <h1>footer</h1>
+      </footer>
+    </div>
+  );
 }
