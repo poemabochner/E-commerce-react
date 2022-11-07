4c0cf15be407a761f814abd149faca174bb92252
Lucas Cardoso da Silva <lucascardoso.ds16@gmail.com>
2022-11-07 17:37:21 -0300

    alteração no header

--- web-react/projeto-final/src/components/Header/index.jsx
+++ web-react/projeto-final/src/components/Header/index.jsx
@@ -1,13 +1,15 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
+
 
 export function Header() {
     return (
         <>
             <nav id='header'>
                 <div className='logo'>
-                    <img className='img-logo' src="https://cdn.icon-icons.com/icons2/851/PNG/512/Shopping_Bag_icon-icons.com_67506.png" alt="Logo-Loja" />
-                    <div className='nome-loja'>
-                        <h1 className='bordas'>Pallet Gym</h1>
+                    <Link to={'/'}><img className='img-logo' src="../favicon.png" alt="Logo-Loja" /></Link>
+                    <div>
+                        <Link to={'/'} className='nome-loja'>Pallet PokeMart</Link>
                     </div>
                 </div>
 
@@ -16,17 +18,25 @@ export function Header() {
                 </div> */}
 
                 <div className='header-direita'>
-                    <img className='img-carrinho' src="https://cdn.icon-icons.com/icons2/851/PNG/512/Bag_icon-icons.com_67588.png" alt="Carrinho" />
-                    <h1 className='bordas'>Produtos</h1>
+                    <div className='box-direita'>
+                        <Link to={'/carrinho'}>
+                            <img className='img-carrinho' src="https://cdn.icon-icons.com/icons2/851/PNG/512/Bag_icon-icons.com_67588.png" alt="Carrinho" />
+                        </Link>
+                        <Link to={'/carrinho'} className='carrinho'>Carrinho</Link>
+                        <a href="https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_National_Pokédex_number">
+                            <img className='img-pokedex' src="https://cdn-icons-png.flaticon.com/512/188/188940.png" alt="Pokedex" />
+                        </a>
+                        <a href="https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_National_Pokédex_number" className='pokedex'>Pokedex</a>
+                    </div>
                 </div>
             </nav >
             <nav id='sub-header'>
                 <div className='categoria-header'>
-                    <button className='bordas'>Categoria</button>
-                    <h1 className='bordas'>Categoria</h1>
-                    <h1 className='bordas'>Categoria</h1>
-                    <h1 className='bordas'>Categoria</h1>
-                    <h1 className='bordas'>Categoria</h1>
+                    <Link to={'/'} className='links'>Pokebolas</Link>
+                    <Link to={'/'} className='links'>Item</Link>
+                    <Link to={'/'} className='links'>Categoria</Link>
+                    <Link to={'/'} className='links'>Categoria</Link>
+                    <Link to={'/'} className='links'>Categoria</Link>
                 </div>
             </nav>
         </>
