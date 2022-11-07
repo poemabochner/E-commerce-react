33b3084d5bd5febdd3afacef97b6a898643584b5
GabrielFsimoes <gabrielsimoes703@gmail.com>
2022-11-07 20:00:08 -0300

    Correção css page produtos

--- web-react/projeto-final/src/components/Footer/index.jsx
+++ web-react/projeto-final/src/components/Footer/index.jsx
@@ -15,15 +15,15 @@ export const Footer = () =>{
                 <Link to={'/carrinho'} className='link'>Carrinho</Link>
             </div>
             <div id='icons'>
-                <Link to={'https://github.com/BilPinheiro/E-commerce-react'}>
+                <a href='https://github.com/BilPinheiro/E-commerce-react'>
                 <img className='icones' src="/src/assets/whats.svg" alt="WhatsApp logo" />
-                </Link>
-                <Link to={'https://github.com/BilPinheiro/E-commerce-react'}>
+                </a>
+                <a href='https://github.com/BilPinheiro/E-commerce-react'>
                 <img className='icones' src="/src/assets/github.svg" alt="Github logo"></img>
-                </Link>
-                <Link to={'https://github.com/BilPinheiro/E-commerce-react'}>
+                </a>
+                <a href='https://github.com/BilPinheiro/E-commerce-react'>
                 <img className='icones' src="/src/assets/linkedin.svg" alt="linkedin logo" />
-                </Link>
+                </a>
             </div>
             <div id='CopyRight'>
                 <p>Copyright @2022 - Grupo 2 All rights reserved | Serratec Residencia em TIC Software</p>
@@ -31,4 +31,3 @@ export const Footer = () =>{
         </footer>
     )
 }
-
