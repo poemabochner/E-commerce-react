dc7a5e923514ad66a6f8188e83a214cd9f26a29e
poemabochner <poemabochner8@gmail.com>
2022-11-08 20:17:15 -0300

    login e senha

--- web-react/projeto-final/src/pages/Login/index.jsx
+++ web-react/projeto-final/src/pages/Login/index.jsx
@@ -3,9 +3,36 @@ import "./style.css";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Header } from "../../components/Header";
+import api from "../../services/api";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 
 function Login() {
+  const [login, setLogin] = useState();
+  const [senha, setSenha] = useState();
+  const [usuario, setUsuario] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const usuarios = async() => {
+      const res = await api.get('/usuario');
+      setUsuario(res.data);
+    }
+    usuarios();
+    }, []);
+
+    function verificaLogin(){
+      usuario.map((u) => { 
+        if(u.email == login){
+         if(u.senha == senha){
+          navigate('/');
+        }
+    }
+    alert("E-mail ou Senha inválidos");
+  })
+}
+
   return (
     <>
       <header>
@@ -21,15 +48,15 @@ function Login() {
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>e-mail</Form.Label>
-              <Form.Control type="email" placeholder="digite seu e-mail" />
+              <Form.Control type="email" placeholder="digite seu e-mail" onChange={(e)=>setLogin(e.target.value)}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>senha</Form.Label>
-              <Form.Control type="password" placeholder="digite sua senha" />
+              <Form.Control type="password" placeholder="digite sua senha" onChange={(e)=>setSenha(e.target.value)}/>
             </Form.Group>
 
             <div className="button">
-              <Button className="button1">
+              <Button className="button1" onClick={verificaLogin}>
                 Continuar
               </Button>
             </div>
