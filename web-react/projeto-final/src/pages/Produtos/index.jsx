1e32bf9364c0c91c6dc2e3110ca0593dd878e11f
Lucas Cardoso da Silva <lucascardoso.ds16@gmail.com>
2022-11-08 16:13:21 -0300

    correção bug footer branco

--- web-react/projeto-final/src/pages/Produtos/index.jsx
+++ web-react/projeto-final/src/pages/Produtos/index.jsx
@@ -1,11 +1,10 @@
-import { CardProduto } from "../../components/CardProduto";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
-import { Link } from "react-router-dom";
-import "./styles.css";
-import api from "../../services/api";
-import { useState, useEffect } from "react";
 import { ModalCards } from "../../components/Modal";
+import api from "../../services/api";
+import "./styles.css";
 
 export function Produtos() {
   const [produtos, setProdutos] = useState([]);
@@ -41,32 +40,32 @@ export function Produtos() {
   }, [filtro]);
 
   function mudaFiltro() {
-    switch(v){
+    switch (v) {
       case 0:
-      setFiltro(0)
-      break;
+        setFiltro(0)
+        break;
       case 1:
-      setFiltro(1)
-      break;
+        setFiltro(1)
+        break;
       case 2:
-      setFiltro(2)
-      break;
+        setFiltro(2)
+        break;
       case 3:
-      setFiltro(3)
-      break;
+        setFiltro(3)
+        break;
       case 4:
-       setFiltro(4)
-       break;
+        setFiltro(4)
+        break;
       case 5:
         setFiltro(5)
         break;
     }
   }
-    
- 
 
-  
-   return (
+
+
+
+  return (
     <div className="fundo">
       <header>
         <Header />
@@ -74,38 +73,38 @@ export function Produtos() {
       <div className="Colunas">
         <asside className="col">
           <div className="texto">
-            <Link className="filtroColuna" onClick={() => {v =1; mudaFiltro();}}>Poke-Bolas</Link>
-            <div className="linksColuna">
-            {/* <Link className="linkC">Mais vendidos</Link> */}
-            </div>
-          </div>
-          <div className="texto">
-            <Link className="filtroColuna" onClick={() => {v =2; mudaFiltro();}}>Medicinas</Link>
+            <Link className="filtroColuna" onClick={() => { v = 1; mudaFiltro(); }}>Poke-Bolas</Link>
             <div className="linksColuna">
               {/* <Link className="linkC">Mais vendidos</Link> */}
             </div>
           </div>
           <div className="texto">
-            <Link className="filtroColuna" onClick={() => {v =3; mudaFiltro();}}>incensos</Link>
+            <Link className="filtroColuna" onClick={() => { v = 2; mudaFiltro(); }}>Medicinas</Link>
+            <div className="linksColuna">
+              {/* <Link className="linkC">Mais vendidos</Link> */}
+            </div>
+          </div>
+          <div className="texto">
+            <Link className="filtroColuna" onClick={() => { v = 3; mudaFiltro(); }}>incensos</Link>
             <div className="linksColuna">
               {/* <Link to={"/links"} className="linkC">Mais vendidos</Link> */}
             </div>
           </div>
           <div className="texto">
-            <Link className="filtroColuna" onClick={() => {v =4; mudaFiltro();}}>Itens de Batalha</Link>
+            <Link className="filtroColuna" onClick={() => { v = 4; mudaFiltro(); }}>Itens de Batalha</Link>
             <div className="linksColuna">
               {/* <Link to={"/links"} className="linkC">Mais vendidos</Link> */}
             </div>
           </div>
           <div className="texto">
-            <Link  className="filtroColuna" onClick={() => {v =5; mudaFiltro();}}>Mints</Link>
+            <Link className="filtroColuna" onClick={() => { v = 5; mudaFiltro(); }}>Mints</Link>
             <div className="linksColuna">
               {/* <Link to={"/links"} className="linkC">Mais vendidos</Link> */}
             </div>
           </div>
           <div className="texto">
-            <Link className="filtroColuna" onClick={() => {v =0; mudaFiltro();}}>Todos</Link>
-           </div>
+            <Link className="filtroColuna" onClick={() => { v = 0; mudaFiltro(); }}>Todos</Link>
+          </div>
         </asside>
         <div className="col2">
           <div className="boxDescricao">
